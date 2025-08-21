e3eff4759fea884ca331fd3ae46d1924f14af21c
Anubhaw-Singh <vkt94594@gmail.com>
2025-08-21 23:58:23 +0530

    using ngOninit to load appointments from localstorage and adding bootstrap

--- appointment-app/src/app/appointment-list/appointment-list.component.ts
+++ appointment-app/src/app/appointment-list/appointment-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
 import {FormsModule} from '@angular/forms'
 import { CommonModule } from '@angular/common';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
@@ -9,10 +10,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.css'
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
+
   newAppointmentTitle : string = "";
   newAppointmentDate : Date = new Date();
   appointments: Appointment[] = [];
+
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem("appointments");
+
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : [];
+    
+  }
 
   addAppointment(){
     if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
